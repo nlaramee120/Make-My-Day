d8e254222f5a0af6ef80e4590fc21c43c8eb6d99
nlaramee120 <nlaramee120@aol.com>
2021-09-09 01:17:07 -0500

    Created a function to fetch the data when the search bar is entered. Created 2 different ways to organize the data. Could not figure out how to implement the user's input with the API.

--- js/seatgeek.js
+++ js/seatgeek.js
@@ -1,7 +1,12 @@
-var url = 'https://api.seatgeek.com/2/events?client_id=MjMyNTQ3OTl8MTYzMTEyNjQ3Mi40MjI1NzMz';
+var userInput = document.querySelector(".userInput");
+var userBtn = document.querySelector(".userBtn");
 
-function seatGeek () {
-// e.preventDefault();
+
+var url =
+  "https://api.seatgeek.com/2/events?client_id=MjMyNTQ3OTl8MTYzMTEyNjQ3Mi40MjI1NzMz";
+
+function seatGeek() {
+  // e.preventDefault();
 
   fetch(url, {
     method: "GET",
@@ -10,36 +15,106 @@ function seatGeek () {
       return data.json();
     })
     .then(function (data) {
-        console.log(data);
-    
-    for (i = 0; i < 5; i++) {    
+      console.log(data);
 
+      for (i = 0; i < 5; i++) {
         var newCard = $("<div>").attr("class", "card");
         $(".nearEvents").append(newCard);
-        var newCardBody = $("<div>").attr("class", "card-body")
-        $(".card").append(newCardBody)
+        var newCardBody = $("<div>").attr("class", "card-body");
+        $(".card").append(newCardBody);
         console.log("it works");
 
         var img = data.events[i].performers[0].image;
         // var imgDisplay = "https://api.seatgeek.com/2/events" + img;
-        newCardBody.append("<img class='img-fluid mb-2' src=" + "'" + img + "'" + "alt=" + "test" + "></img>");
+        newCardBody.append(
+          "<img class='img-fluid mb-2' src=" +
+            "'" +
+            img +
+            "'" +
+            "alt=" +
+            "test" +
+            "></img>"
+        );
 
         var title = data.events[i].short_title;
         newCardBody.append("<h3 class='card-title'>" + title + "<h5>");
 
         var date = data.events[i].datetime_local;
-        newCardBody.append("<p class='badge badge-secondary'>" + "Date: " + date + "<p>");
+        newCardBody.append(
+          "<p class='badge badge-secondary'>" + "Date: " + date + "<p>"
+        );
 
         var location = data.events[i].venue.name;
-        newCardBody.append("<p class='badge badge-secondary'>" + "Location: " + location + "<p>");
+        newCardBody.append(
+          "<p class='badge badge-secondary'>" + "Location: " + location + "<p>"
+        );
 
         var getTicks = data.events[i].url;
-        newCardBody.append("<a class='btn btn-primary btn-dlock mt-4' target='_blank' href=" + getTicks + ">Get Tickets!" + "</a>");
+        newCardBody.append(
+          "<a class='btn btn-primary btn-dlock mt-4' target='_blank' href=" +
+            getTicks +
+            ">Get Tickets!" +
+            "</a>"
+        );
 
-        newCardBody.append("<button type='button' class='btn btn-primary btn-dlock mt-4'>" + "Save Event!" + "</button>");
-
-    }
-    })
+        newCardBody.append(
+          "<button type='button' class='btn btn-primary btn-dlock mt-4'>" +
+            "Save Event!" +
+            "</button>"
+        );
+      }
+    });
 }
 
 seatGeek();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var urlSearchBar = "https://api.seatgeek.com/2/events?geoip=true&range=100mi&datetime_local.gt=2021-09-09&client_id=MjMyNTQ3OTl8MTYzMTEyNjQ3Mi40MjI1NzMz"
+
+var urlSearchBar = "https://api.seatgeek.com/2/events?q=" + userInput.value + "&client_id=MjMyNTQ3OTl8MTYzMTEyNjQ3Mi40MjI1NzMz"
+
+userBtn.addEventListener("click", () => {
+fetch(urlSearchBar, {
+  method: "GET",
+})
+  .then(function(data) {
+    return data.json()
+  })
+  .then(function (data) {
+    console.log(data)
+
+
+  })
+})
