da8a4be8e90718f9330d1dbcf214a30e0af7dea6
James Capadocia <james.capadocia@gmail.com>
2021-09-09 15:16:48 -0500

    Implemented OpenWeather API. It semi-works.

--- js/seatgeek.js
+++ js/seatgeek.js
@@ -4,8 +4,200 @@ var url = 'https://api.seatgeek.com/2/events?geoip=true&range=20mi&client_id=MjM
 
 
 
-var url =
-  "https://api.seatgeek.com/2/events?client_id=MjMyNTQ3OTl8MTYzMTEyNjQ3Mi40MjI1NzMz";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function seatGeek() {
   // e.preventDefault();
@@ -28,15 +220,7 @@ function seatGeek() {
 
         var img = data.events[i].performers[0].image;
         // var imgDisplay = "https://api.seatgeek.com/2/events" + img;
-        newCardBody.append(
-          "<img class='img-fluid mb-2' src=" +
-            "'" +
-            img +
-            "'" +
-            "alt=" +
-            "test" +
-            "></img>"
-        );
+        newCardBody.append("<img class='img-fluid mb-2' src=" + "'" + img + "'" + "alt=" + "test" + "></img>");
 
         var title = data.events[i].short_title;
         newCardBody.append("<h3 class='card-title'>" + title + "<h5>");
@@ -49,21 +233,46 @@ function seatGeek() {
         newCardBody.append("<p class='card-text'>" + "Location: " + location + "<p>");
 
         var getTicks = data.events[i].url;
-        newCardBody.append(
-          "<a class='btn btn-primary btn-dlock mt-4' target='_blank' href=" +
-            getTicks +
-            ">Get Tickets!" +
-            "</a>"
-        );
+        newCardBody.append("<a class='btn btn-primary btn-dlock mt-4' target='_blank' href=" + getTicks + ">Get Tickets!" + "</a>");
 
-        newCardBody.append(
-          "<button type='button' class='btn btn-primary btn-dlock mt-4'>" +
-            "Save Event!" +
-            "</button>"
+        newCardBody.append("<button type='button' class='btn btn-primary btn-dlock mt-4'>" + "Save Event!" + "</button>"
         );
       }
     });
-}
+  }
+
+
+
+  var openWeatherKey = "3a150e01056da8ad0b1ee8083da97feb";
+  var openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=${openWeatherKey}&units=imperial`;
+
+  fetch(openWeatherUrl, {
+    method: "GET",
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    // .then(function (response) {
+    //   console.log(response);
+    // })
+    .then(function (response) {
+      var newCard = $(".card-body").append("<div class='col-sm-7 bg-primary text-white rounded'></div>");
+      // newCard.empty();
+
+      var currentName = newCard.append("<p>");
+      newCard.append(currentName);
+
+      var time = new Date(response.dt * 1000);
+      currentName.append(response.name + " " + time.toLocaleDateString("en-US"));
+      currentName.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
+      
+      var currentWeather = currentName.append("<p>");
+      currentName.append(currentWeather);
+      currentWeather.append("<p>" + "Temperature: " + response.main.temp + " F" + "</p>");
+      currentWeather.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
+      currentWeather.append("<p>" + "Wind: " + response.wind.speed + " mph" + "</p>");
+    })
+
 
 seatGeek();
 
@@ -116,4 +325,4 @@ fetch(urlSearchBar, {
 
 
   })
-})
+});
