4ff161d2fd1501e870f6d1dd5f33ef4a57acd939
nlaramee120 <nlaramee120@aol.com>
2021-09-10 18:05:56 -0500

    Included a template to potential load user's local weather on page load.

--- js/seatgeek.js
+++ js/seatgeek.js
@@ -3,6 +3,7 @@ $(document).ready(function()  {
 var userInput = $(".userInput").val();
 var userBtn = document.querySelector(".userBtn");
 var url = 'https://api.seatgeek.com/2/events?geoip=true&range=100mi&client_id=MjMyNTQ3OTl8MTYzMTEyNjQ3Mi40MjI1NzMz';
+// var urlGeo = `https://api.openweathermap.org/data/2.5/weather?q=&lat=${response.coord.lat}&lon=${response.coord.lon}&appid=3a150e01056da8ad0b1ee8083da97feb&units=imperial`;
 var weatherKey = "3a150e01056da8ad0b1ee8083da97feb";
 
 
@@ -156,7 +157,37 @@ function seatGeek() {
         );
       }
     });
+    
+    
+  //   fetch(urlGeo,  {
+  //     method: "GET",
+  //   })
+  //   .then(function (response) {
+  //     return response.json()
+  //   })
+  //   .then(function (response) {
+     
+  //   })
 
+  //   .then(function (response) {
+      
+  //      $(".appendedAll").remove();
+
+  //           for (i = 0; i < 5; i++) {
+  //             var newItem = $("<div>").attr("class", "col-sm-12 bg-primary text-white rounded appendedAll");
+  //             $(".fiveDay").append(newItem);
+      
+  //             var date = new Date(response.list[i * 8].dt * 1000);
+  //             newItem.append("<h4>" + date.toLocaleDateString() + "<h4>");
+      
+  //             var iconCode = response.list[i * 8].weather[0].icon;
+  //             var iconURL = "http://openweathermap.org/img/wn/" + iconCode + ".png";
+  //             newItem.append($("<img>").attr("src", iconURL));
+      
+  //             var temp = response.list[i * 8].main.temp;
+  //             newItem.append("<p>" + ("Temp: " + temp + " F") + "<p>");
+  //   }
+  // })
     
 }
 
