d41c790dd55c24f0ec9b33bd32484708bde9bec3
James Capadocia <james.capadocia@gmail.com>
2021-09-09 23:18:53 -0500

    Created a five day forecast column and fetch to represent weather.

--- js/seatgeek.js
+++ js/seatgeek.js
@@ -72,37 +72,75 @@ fetch(urlSearchBar, {
 
 
 
+// var searchButton = $("#searchButton");
+// var city = $("#searchInput").val();
+var key = "3a150e01056da8ad0b1ee8083da97feb";
 
+function fiveDay() {
+  var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=chicago" + "&appid=" + key + "&units=imperial";
+  // e.preventDefault();
 
-var openWeatherKey = "3a150e01056da8ad0b1ee8083da97feb";
-var openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=${openWeatherKey}&units=imperial`;
-
-fetch(openWeatherUrl, {
-  method: "GET",
-})
-  .then(function (response) {
-    return response.json();
+  // Begin API fetch and appending of data retrieved
+  fetch(urlFiveDay, {
+    method: "GET",
   })
-  // .then(function (response) {
-  //   console.log(response);
-  // })
-  .then(function (response) {
-    var newCard = $(".card-body").append("<div class='col-sm-7 bg-primary text-white rounded'></div>");
-    // newCard.empty();
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      $(".appendedAll").remove();
 
-    var currentName = newCard.append("<p>");
-    newCard.append(currentName);
+      // For loop to create and append data retrieved for the next consecutive 5 days in separate <divs>
+      for (i = 0; i < 5; i++) {
+        var newItem = $("<div>").attr("class", "col-sm-2 bg-primary text-white rounded appendedAll");
+        $(".fiveDay").append(newItem);
 
-    var time = new Date(response.dt * 1000);
-    currentName.append(response.name + " " + time.toLocaleDateString("en-US"));
-    currentName.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
+        var date = new Date(response.list[i * 8].dt * 1000);
+        newItem.append("<h4>" + date.toLocaleDateString() + "<h4>");
 
-    var currentWeather = currentName.append("<p>");
-    currentName.append(currentWeather);
-    currentWeather.append("<p>" + "Temperature: " + response.main.temp + " F" + "</p>");
-    currentWeather.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
-    currentWeather.append("<p>" + "Wind: " + response.wind.speed + " mph" + "</p>");
-  })
+        var iconCode = response.list[i * 8].weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/wn/" + iconCode + ".png";
+        newItem.append($("<img>").attr("src", iconURL));
+
+        var temp = response.list[i * 8].main.temp;
+        newItem.append("<p>" + ("Temp: " + temp + " F") + "<p>");
+      }
+    })
+};
+
+fiveDay();
+
+
+
+// var openWeatherKey = "3a150e01056da8ad0b1ee8083da97feb";
+// var openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=${openWeatherKey}&units=imperial`;
+
+// fetch(openWeatherUrl, {
+//   method: "GET",
+// })
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   // .then(function (response) {
+//   //   console.log(response);
+//   // })
+//   .then(function (response) {
+//     var newCard = $(".card-body").append("<div class='col-sm-7 bg-primary text-white rounded'></div>");
+//     // newCard.empty();
+
+//     var currentName = newCard.append("<p>");
+//     newCard.append(currentName);
+
+//     var time = new Date(response.dt * 1000);
+//     currentName.append(response.name + " " + time.toLocaleDateString("en-US"));
+//     currentName.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
+
+//     var currentWeather = currentName.append("<p>");
+//     currentName.append(currentWeather);
+//     currentWeather.append("<p>" + "Temperature: " + response.main.temp + " F" + "</p>");
+//     currentWeather.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
+//     currentWeather.append("<p>" + "Wind: " + response.wind.speed + " mph" + "</p>");
+//   })
 
 
 
